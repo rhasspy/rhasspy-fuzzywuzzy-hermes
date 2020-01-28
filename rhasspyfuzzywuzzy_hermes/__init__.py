7b29f79aa68000f079f5b7353092fe1a40a5b0a1
Michael Hansen <michael.hansen.24@us.af.mil>
2020-01-28 17:10:51 -0500

    Using rhasspy-fuzzywuzzy library

--- rhasspyfuzzywuzzy_hermes/__init__.py
+++ rhasspyfuzzywuzzy_hermes/__init__.py
@@ -1,17 +1,14 @@
 """Hermes MQTT server for Rhasspy fuzzywuzzy"""
-import io
 import json
 import logging
-import re
-import time
 import typing
-from collections import defaultdict
 from pathlib import Path
 
 import attr
-import fuzzywuzzy.process
 import networkx as nx
+import rhasspyfuzzywuzzy
 import rhasspynlu
+from rhasspyfuzzywuzzy.const import ExamplesType
 from rhasspyhermes.base import Message
 from rhasspyhermes.intent import Intent, Slot, SlotRange
 from rhasspyhermes.nlu import (
@@ -22,10 +19,7 @@ from rhasspyhermes.nlu import (
     NluTrain,
     NluTrainSuccess,
 )
-from rhasspynlu.intent import Recognition
 from rhasspynlu.jsgf import Sentence
-
-from .utils import generate_examples
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,7 +34,7 @@ class NluHermesMqtt:
         client,
         intent_graph: typing.Optional[nx.DiGraph] = None,
         intent_graph_path: typing.Optional[Path] = None,
-        examples: typing.Optional[typing.Dict[str, typing.List[int]]] = None,
+        examples: typing.Optional[ExamplesType] = None,
         examples_path: typing.Optional[Path] = None,
         sentences: typing.Optional[typing.List[Path]] = None,
         slots_dirs: typing.Optional[typing.List[Path]] = None,
@@ -100,7 +94,7 @@ class NluHermesMqtt:
                     return intent_name in query.intentFilter
                 return True
 
-            recognitions = NluHermesMqtt.recognize(
+            recognitions = rhasspyfuzzywuzzy.recognize(
                 query.input,
                 self.intent_graph,
                 self.examples,
@@ -154,61 +148,6 @@ class NluHermesMqtt:
 
     # -------------------------------------------------------------------------
 
-    @classmethod
-    def recognize(
-        cls,
-        input_text: str,
-        intent_graph: nx.DiGraph,
-        examples: typing.Dict[str, typing.Dict[str, typing.List[int]]],
-        intent_filter: typing.Optional[typing.Callable[[str], bool]] = None,
-        replace_numbers: bool = True,
-        language: str = "en",
-        extra_converters: typing.Optional[
-            typing.Dict[str, typing.Callable[..., typing.Any]]
-        ] = None,
-    ) -> typing.List[Recognition]:
-        """Find the closest matching intent(s)."""
-        start_time = time.perf_counter()
-
-        if replace_numbers:
-            # 75 -> seventy five
-            words = rhasspynlu.numbers.replace_numbers(
-                input_text.split(), language=language
-            )
-
-            input_text = " ".join(words)
-
-        # TODO: Add cache
-        intent_filter = intent_filter or (lambda i: True)
-        choices: typing.Dict[str, typing.List[int]] = {
-            text: path
-            for intent_name, paths in examples.items()
-            for text, path in paths.items()
-            if intent_filter(intent_name)
-        }
-
-        # Find closest match
-        best_text, best_score = fuzzywuzzy.process.extractOne(
-            input_text, choices.keys()
-        )
-        _LOGGER.debug("input=%s, match=%s, score=%s", input_text, best_text, best_score)
-        best_path = choices[best_text]
-
-        end_time = time.perf_counter()
-        _, recognition = rhasspynlu.fsticuffs.path_to_recognition(
-            best_path, intent_graph, extra_converters=extra_converters
-        )
-
-        assert recognition
-        recognition.intent.confidence = best_score / 100
-        recognition.recognize_seconds = end_time - start_time
-        recognition.raw_text = input_text
-        recognition.raw_tokens = input_text.split()
-
-        return [recognition]
-
-    # -------------------------------------------------------------------------
-
     def handle_train(
         self, message: NluTrain, siteId: str = "default"
     ) -> typing.Union[NluTrainSuccess, NluError]:
@@ -216,100 +155,31 @@ class NluHermesMqtt:
         _LOGGER.debug("<- %s", message)
 
         try:
-            self.intent_graph, self.examples = NluHermesMqtt.train(
+            self.intent_graph, self.examples = rhasspyfuzzywuzzy.train(
                 message.sentences,
-                intent_graph_path=self.intent_graph_path,
-                examples_path=self.examples_path,
                 slots_dirs=self.slots_dirs,
                 slot_programs_dirs=self.slot_programs_dirs,
             )
+
+            if self.examples_path:
+                # Write examples to JSON file
+                with open(self.examples_path, "w") as examples_file:
+                    json.dump(self.examples, examples_file)
+
+                _LOGGER.debug("Wrote %s", str(self.examples_path))
+
+            if self.intent_graph_path:
+                # Write graph to JSON file
+                with open(self.intent_graph_path, "w") as graph_file:
+                    graph_dict = rhasspynlu.graph_to_json(self.intent_graph)
+                    json.dump(graph_dict, graph_file)
+
+                _LOGGER.debug("Wrote %s", str(self.intent_graph_path))
 
             return NluTrainSuccess(id=message.id)
         except Exception as e:
             _LOGGER.exception("handle_train")
             return NluError(siteId=siteId, error=str(e), context=message.id)
-
-    # -------------------------------------------------------------------------
-
-    @classmethod
-    def train(
-        cls,
-        sentences: typing.Dict[str, str],
-        intent_graph_path: typing.Optional[Path] = None,
-        examples_path: typing.Optional[Path] = None,
-        slots_dirs: typing.Optional[typing.List[Path]] = None,
-        slot_programs_dirs: typing.Optional[typing.List[Path]] = None,
-        replace_numbers: bool = True,
-        word_casing="ignore",
-    ) -> typing.Tuple[nx.DiGraph, typing.Dict[str, typing.Dict[str, typing.List[int]]]]:
-        """Transform sentences to an intent graph and examples"""
-        # Parse sentences and convert to graph
-        with io.StringIO() as ini_file:
-            # Join as single ini file
-            for lines in sentences.values():
-                print(lines, file=ini_file)
-                print("", file=ini_file)
-
-            # Parse JSGF sentences
-            intents = rhasspynlu.parse_ini(ini_file.getvalue())
-
-        # Split into sentences and rule/slot replacements
-        sentences, replacements = rhasspynlu.ini_jsgf.split_rules(intents)
-
-        if replace_numbers:
-            # Replace number ranges with slot references
-            for intent_sentences in sentences.values():
-                for sentence in intent_sentences:
-                    rhasspynlu.jsgf.walk_expression(
-                        sentence, rhasspynlu.number_range_transform, replacements
-                    )
-
-        # Load slot values
-        # TODO: Add word_transform
-        slot_replacements = rhasspynlu.get_slot_replacements(
-            intents, slots_dirs=slots_dirs, slot_programs_dirs=slot_programs_dirs
-        )
-
-        # Merge with existing replacements
-        for slot_key, slot_values in slot_replacements.items():
-            replacements[slot_key] = slot_values
-
-        if replace_numbers:
-            # Do single number transformations
-            for intent_sentences in sentences.values():
-                for sentence in intent_sentences:
-                    rhasspynlu.jsgf.walk_expression(
-                        sentence, rhasspynlu.number_transform, replacements
-                    )
-
-        # Convert to directed graph
-        intent_graph = rhasspynlu.intents_to_graph(sentences, replacements=replacements)
-        if intent_graph_path:
-            with open(intent_graph_path, "w") as graph_file:
-                graph_dict = rhasspynlu.graph_to_json(intent_graph)
-                json.dump(graph_dict, graph_file)
-
-            _LOGGER.debug("Wrote %s", str(intent_graph_path))
-
-        # Generate all possible intents
-        _LOGGER.debug("Generating examples")
-        examples: typing.Dict[str, typing.Dict[str, typing.List[int]]] = defaultdict(
-            dict
-        )
-        for intent_name, words, path in generate_examples(intent_graph):
-            sentence = " ".join(words)
-            examples[intent_name][sentence] = path
-
-        _LOGGER.debug("Examples generated")
-
-        if examples_path:
-            # Write to JSON file
-            with open(examples_path, "w") as examples_file:
-                json.dump(examples, examples_file)
-
-            _LOGGER.debug("Wrote %s", str(examples_path))
-
-        return (intent_graph, examples)
 
     # -------------------------------------------------------------------------
 
